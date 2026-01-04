e0f2faddb6d8ec0228b734bbd932eecd026310ca
Davide Trabona <65053718+dt-gamesplays@users.noreply.github.com>
2026-01-04 02:36:27 +0100

    Add files via upload

--- hooks/CoreEnvironmentControllerManager.lua
+++ hooks/CoreEnvironmentControllerManager.lua
@@ -1,22 +1,38 @@
+local settings = nil
 
 ----------------------------------------------------------------------
--- ANTI-FLASHBANGS
+-- 1. ANTI-FLASHBANGS
 ----------------------------------------------------------------------
 local old_set_flashbang = CoreEnvironmentControllerManager.set_flashbang
 function CoreEnvironmentControllerManager:set_flashbang(...)
-    if _G.GodMode and _G.GodMode.settings and _G.GodMode.settings.master_switch and _G.GodMode.settings.no_flash then
+    if not settings then
+        if _G.GodMode then
+            settings = _G.GodMode.settings
+        end
+    end
+
+    if settings and settings.master_switch and settings.no_flash then
         return
     end
+
     return old_set_flashbang(self, ...)
 end
 
+
 ----------------------------------------------------------------------
--- ANTI-DISTORTION
+-- 2. ANTI-DISTORTION
 ----------------------------------------------------------------------
 local old_set_concussion = CoreEnvironmentControllerManager.set_concussion_grenade
 function CoreEnvironmentControllerManager:set_concussion_grenade(...)
-    if _G.GodMode and _G.GodMode.settings and _G.GodMode.settings.master_switch and _G.GodMode.settings.no_flash then
+    if not settings then
+        if _G.GodMode then
+            settings = _G.GodMode.settings
+        end
+    end
+
+    if settings and settings.master_switch and settings.no_flash then
         return
     end
+
     return old_set_concussion(self, ...)
 end
